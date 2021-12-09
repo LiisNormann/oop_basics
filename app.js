5e3302710f25728d3475ecf586f1bdef0806caf8
Liis Normann <liis.normann@gmail.com>
2021-12-09 08:35:05 +0000

    OOP test - OOJS1

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 const anna = new Person('Anna', 'Karutina')
 console.log(anna)
 anna.greeting()
@@ -12,5 +13,29 @@ urmas.greeting()
 let liis = new Student('Liis', 'Normann')
 console.log(liis)
 liis.greeting()
+ */
+
+// Shape constructor
+function Shape(name, sides, sideLength) {
+    // atributes - fields
+    this.name = name;
+    this.sides = sides;
+    this.sideLength = sideLength;
+    // functions - methods
+    this.calcPerimeter = function(){
+        // console.log(this.sides * this.sideLength)
+        this.perimeter = this.sides * this.sideLength
+        console.log(this.perimeter)
+    }
+}
+
+// object creation
+let square = new Shape("square", 4, 5)
+console.log(square)
+let triangle = new Shape("triangle", 3, 3)
+console.log(triangle)
+// object method calling
+square.calcPerimeter()
+triangle.calcPerimeter() 
 
 
