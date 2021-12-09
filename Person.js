7d80fa2c0b1b1b30c309b5136c102525087e8361
Liis Normann <liis.normann@gmail.com>
2021-12-09 07:43:06 +0000

    Created Person class getter and setter

--- Person.js
+++ Person.js
@@ -8,4 +8,12 @@ class Person{
     greeting(){
         console.log("Hi!, I'm " + this.firstname + " " + this.lastname + "!")
     }
+
+    setAge(age) {
+        this.age = age;
+    }
+
+    getAge() {
+        return this.age;
+    }
 }
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const anna = new Person('Anna', 'Karutina')
 console.log(anna)
 anna.greeting()
+anna.setAge(25)
+console.log(anna.getAge())
 const urmas = new Person('Urmas', 'Voevodin')
 console.log(urmas)
 urmas.greeting()
